c2610fcf80b9c5e05cec5c42613bfe6f2f327ea0
Carmezim <carmezim.filho@gmail.com>
2017-05-21 20:33:45 +0300

    removing duplicate files

--- Rollup/src/components/createMap.js
+++
@@ -1,6 +0,0 @@
-import {mapOptions} from './options';
-
-const createMap = () => new google.maps.Map(document.getElementById("map"), mapOptions);
-
-export default createMap;
-
--- Rollup/src/components/defineCenter.js
+++
@@ -1,3 +0,0 @@
-const defineCenter = (lat, lng) => new google.maps.LatLng(parseFloat(lat).toFixed(6), parseFloat(lng).toFixed(6));
-
-export default defineCenter;
